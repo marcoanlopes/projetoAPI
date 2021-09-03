64c218a3b0502fcd533a566363a65edac908defe
Marco Lopes <marcoalopes@outlook.com>
2021-09-02 22:42:13 -0300

    Adição do delete e getbyid

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -18,17 +18,52 @@ namespace API.Controllers
         //POST: api/produto/create
         [HttpPost]
         [Route("create")]
-        public Produto Create(Produto produto)
+        public IActionResult Create([FromBody] Produto produto)
         {
             _context.Produtos.Add(produto);
             _context.SaveChanges();
-            return produto;
+            return Created("", produto);
         }
 
         //get: api/produto/list
         [HttpGet]
         [Route("list")]
-        public List<Produto> List() => _context.Produtos.ToList();
+        public IActionResult List() => Ok(_context.Produtos.ToList());
+
+        [HttpGet]
+        [Route("getById/{id}")]
+        public IActionResult FindById([FromRoute]int id){
+            Produto produto = _context.Produtos.Find(id);
+            if(produto == null){
+                return NotFound();
+            }
+
+            return Ok(produto);
+        //    return _context.Produtos.SingleOrDefault(produtoId => produtoId.Id == id);
+        }
+
+        // [HttpPut]
+        // [Route("update/{id}")]
+        // public Produto Update(int id){
+
+        //     return Produto;
+        // }
+
+        [HttpDelete]
+        [Route("delete/{name}")]
+        public IActionResult Delete([FromRoute] string name){
+            //Expressão lambda
+            //buscar pelo primeiro nome
+            Produto produto = _context.Produtos.FirstOrDefault(x => x.Nome == name);
+            if(produto == null){
+                return NotFound();
+            }
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+            return Ok(_context.Produtos.ToList());
+        }
+
+
 
     }
 
